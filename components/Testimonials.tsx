5262c23bdd10d99b0392817b9c4436a706a40bf2
DrZiMo <zuhaybhamar@gmail.com>
2026-04-24 18:06:52 +0300

    feat: add InfiniteMovingCards component for dynamic testimonial display

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,9 +1,18 @@
+import { testimonials } from '@/data'
 import Heading from './Heading'
+import { InfiniteMovingCards } from './ui/infinite-moving-cards'
 
 const Testimonials = () => {
   return (
     <div className='py-20'>
       <Heading text={'What people say about'} subText={'my work'} />
+      <div className='mt-10'>
+        <InfiniteMovingCards
+          items={testimonials}
+          direction='right'
+          speed='slow'
+        />
+      </div>
     </div>
   )
 }
