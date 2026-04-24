d697971b005770d03b993d2e01f72ef9f7253c99
DrZiMo <zuhaybhamar@gmail.com>
2026-04-24 21:18:27 +0300

    feat: enhance hover effect on SocialMediaFooter component for improved user interaction

--- components/SocialMediaFooter.tsx
+++ components/SocialMediaFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SocialMediaFooter = ({ icon }: { icon: React.ReactNode }) => {
   return (
-    <div className='relative shrink-0 rounded-md border border-b border-zinc-100 px-2 py-2 dark:border-zinc-700 bg-white-100/5'>
+    <div className='relative shrink-0 rounded-md border border-b border-zinc-100 px-2 py-2 dark:border-zinc-700 bg-white-100/5 hover:bg-white-100/15 transition-colors duration-300'>
       {icon}
     </div>
   )
