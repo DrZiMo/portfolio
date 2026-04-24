904a0d4889fb85556986a1e8f7ca60a6b2a7d9a5
DrZiMo <zuhaybhamar@gmail.com>
2026-04-24 21:25:54 +0300

    fix: update copyright year in Footer component to 2026

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
   return (
     <div className='py-10 flex flex-col md:flex-row justify-between items-center'>
       <div className='text-sm text-white-100'>
-        &copy; 2024 Zuhayb. All rights reserved.
+        &copy; 2026 Zuhayb. All rights reserved.
       </div>
       <div className='flex space-x-2 mt-2 md:mt-0'>
         {socialMedia.map((item, index) => (
